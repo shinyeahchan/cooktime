df3ab960d5551778d080c5ccc39f3471d3d6b112
JhyunJung <jhyun.rigel@gmail.com>
2023-06-07 19:13:12 +0900

    Project Init Setup

---
+++ src/main/java/com/side/cooktime/CooktimeApplication.java
@@ -0,0 +1,13 @@
+package com.side.cooktime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CooktimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CooktimeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/side/cooktime/CooktimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.side.cooktime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CooktimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
