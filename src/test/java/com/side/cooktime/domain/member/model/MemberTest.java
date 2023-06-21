4f627f401890d9a91a258b84d71c970d7e1c3a51
ShinYeahChan <syeahc@gmail.com>
2023-06-21 20:42:27 +0900

    refactor : @ParameterizedTest 활용, 테스트코드 개선

--- src/test/java/com/side/cooktime/domain/member/model/MemberTest.java
+++ src/test/java/com/side/cooktime/domain/member/model/MemberTest.java
@@ -1,6 +1,9 @@
 package com.side.cooktime.domain.member.model;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -26,38 +29,29 @@ class MemberTest {
         );
     }
 
-    @Test
-    void email_유효성검사() {
-        assertAll(
-                () -> assertThatThrownBy(() -> new User(null, "password1234", "길동", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new User("", "password1234", "길동", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new User("user", "password1234", "길동", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new User("user@", "password1234", "길동", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class)
-        );
+    @ParameterizedTest(name = "email 입력값: {0}")
+    @NullAndEmptySource
+    @ValueSource(strings = {"user", "user@"})
+    void email_유효성검사(String email) {
+        assertThatThrownBy(() -> new User(email, "password1234", "길동", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void password_유효성검사() {
-        assertAll(
-                () -> assertThatThrownBy(() -> new User("user@gmail.com", null, "길동", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new User("user@gmail.com", "", "길동", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new User("user@gmail.com", "shortPw", "길동", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class)
-        );
+    @ParameterizedTest(name = "password 입력값: {0}")
+    @NullAndEmptySource
+    @ValueSource(strings = {"shortPw"})
+    void password_유효성검사(String password) {
+        assertThatThrownBy(() -> new User("user@gmail.com", password, "길동", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void firstName_유효성검사() {
-        assertAll(
-                () -> assertThatThrownBy(() -> new User("user@gmail.com", "password1234", null, "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new User("user@gmail.com", "password1234", "", "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class)
-        );
+    @ParameterizedTest(name = "firstName 입력값: {0}")
+    @NullAndEmptySource
+    void firstName_유효성검사(String firstName) {
+        assertThatThrownBy(() -> new User("user@gmail.com", "password1234", firstName, "홍", Gender.MALE, 30)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @Test
-    void age_유효성검사() {
-        assertAll(
-                () -> assertThatThrownBy(() -> new User("user@gmail.com", "password1234", "길동", "홍", Gender.MALE, -1)).isInstanceOf(IllegalArgumentException.class),
-                () -> assertThatThrownBy(() -> new User("user@gmail.com", "password1234", "길동", "홍", Gender.MALE, -10)).isInstanceOf(IllegalArgumentException.class)
-        );
+    @ParameterizedTest(name = "age 입력값: {0}")
+    @ValueSource(ints = {-1, -10})
+    void age_유효성검사(int age) {
+        assertThatThrownBy(() -> new User("user@gmail.com", "password1234", "길동", "홍", Gender.MALE, age)).isInstanceOf(IllegalArgumentException.class);
     }
 }
