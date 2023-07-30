f334677be842375ec6c688dbf9369e70d5765c31
ShinYeahChan <syeahc@gmail.com>
2023-07-30 23:12:58 +0900

    fix : USER Role

--- src/main/java/com/side/cooktime/domain/member/model/User.java
+++ src/main/java/com/side/cooktime/domain/member/model/User.java
@@ -16,13 +16,13 @@ public class User extends Member {
     private Age age;
 
     public User(final String provider, final String providerId, final String email, final String password, final String firstName, final String lastName, final String gender, final int age) {
-        super(Role.ADMIN, provider, providerId, email, password, firstName, lastName);
+        super(Role.USER, provider, providerId, email, password, firstName, lastName);
         this.gender = Gender.valueOf(gender.toUpperCase());
         this.age = new Age(age);
     }
 
     public User(final String email, final String password, final String firstName, final String lastName, final Gender gender, final int age) {
-        super(Role.ADMIN, email, password, firstName, lastName);
+        super(Role.USER, email, password, firstName, lastName);
         this.gender = gender;
         this.age = new Age(age);
     }
