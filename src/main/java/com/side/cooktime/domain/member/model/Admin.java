5e17e031a3451573aa80f469ba613d02d467a311
ShinYeahChan <syeahc@gmail.com>
2023-07-30 22:24:48 +0900

    refactor : member 테이블 dtype 컬럼명 변경

--- src/main/java/com/side/cooktime/domain/member/model/Admin.java
+++ src/main/java/com/side/cooktime/domain/member/model/Admin.java
@@ -6,11 +6,11 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "member")
-//@DiscriminatorValue("A")
+@DiscriminatorValue("A")
 public class Admin extends Member {
 
     public Admin(final String email, final String password, final String firstName, final String lastName) {
-        super(email, password, firstName, lastName);
+        super(Role.ADMIN, email, password, firstName, lastName);
     }
 
     protected Admin() {
--- src/main/java/com/side/cooktime/domain/member/model/Member.java
+++ src/main/java/com/side/cooktime/domain/member/model/Member.java
@@ -8,8 +8,7 @@ import lombok.Getter;
 @Getter
 @Table(name = "member")
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE)
-//TODO: Role을 enum타입 대신 쓸 수 있다면
-//@DiscriminatorColumn(name = "role")
+@DiscriminatorColumn(name = "user_type")
 public abstract class Member extends BaseEntity {
 
     @Embedded
@@ -19,23 +18,33 @@ public abstract class Member extends BaseEntity {
     @Embedded
     protected Name name;
 
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role", insertable = false, updatable = false)
+    private Role role;
+
     protected String provider;
     protected String providerId;
 
-    protected Member(final String email, final String password, final String firstName, final String lastName) {
+    protected Member(final Role role, final String email, final String password, final String firstName, final String lastName) {
         super();
+        this.role = role;
         this.email = new Email(email);
         this.password = new Password(password);
         this.name = new Name(firstName, lastName);
     }
 
-    protected Member(final String provider, final String providerId, final String email, final String password, final String firstName, final String lastName) {
+    protected Member(final Role role, final String provider, final String providerId, final String email, final String password, final String firstName, final String lastName) {
         super();
+        this.role = role;
         this.provider = provider;
         this.providerId = providerId;
         this.email = new Email(email);
         this.password = new Password(password);
         this.name = new Name(firstName, lastName);
+    }
+
+    public String getRoleKey() {
+        return role.getKey();
     }
 
     protected Member() {
