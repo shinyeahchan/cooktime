386c69c5d5290850813c76303c577946f3dfcf42
ShinYeahChan <syeahc@gmail.com>
2023-08-08 18:26:44 +0900

    feat : 요청한 사용자 정보 추출 메서드 클래스 분리

--- src/main/java/com/side/cooktime/domain/userstorage/controller/UserStorageController.java
+++ src/main/java/com/side/cooktime/domain/userstorage/controller/UserStorageController.java
@@ -1,6 +1,6 @@
 package com.side.cooktime.domain.userstorage.controller;
 
-import com.side.cooktime.domain.userstorage.model.UserStorage;
+import com.side.cooktime.config.auth.OAuth2UserUtils;
 import com.side.cooktime.domain.userstorage.model.dto.request.RequestDeleteDto;
 import com.side.cooktime.domain.userstorage.model.dto.request.RequestSaveDto;
 import com.side.cooktime.domain.userstorage.model.dto.response.ResponseDeleteDto;
@@ -10,7 +10,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,19 +23,13 @@ public class UserStorageController {
 
     @PostMapping("/storage")
     public ResponseEntity<ResponseSaveDto> save(@RequestBody List<RequestSaveDto> requestDtos, Authentication authentication) {
-        // 현재 인증된 사용자의 정보 가져오기
-        DefaultOAuth2User oauth2User = (DefaultOAuth2User) authentication.getPrincipal();
-        // oauth2User에서 이메일 정보 가져오기
-        String memberEmail = oauth2User.getAttribute("email");
-        ResponseSaveDto responseDto = userStorageService.saveAll(memberEmail, requestDtos);
+        ResponseSaveDto responseDto = userStorageService.saveAll(OAuth2UserUtils.getEmail(authentication), requestDtos);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 
     @PutMapping("/storage/delete")
     public ResponseEntity<ResponseDeleteDto> delete(@RequestBody List<RequestDeleteDto> requestDtos, Authentication authentication) {
-        DefaultOAuth2User oauth2User = (DefaultOAuth2User) authentication.getPrincipal();
-        String memberEmail = oauth2User.getAttribute("email");
-        ResponseDeleteDto responseDto = userStorageService.deleteAllSoftly(memberEmail, requestDtos);
+        ResponseDeleteDto responseDto = userStorageService.deleteAllSoftly(OAuth2UserUtils.getEmail(authentication), requestDtos);
         return new ResponseEntity<>(responseDto, HttpStatus.OK);
     }
 }
