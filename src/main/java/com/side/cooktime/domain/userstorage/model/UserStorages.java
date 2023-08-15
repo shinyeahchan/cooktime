73886a7faaa8710231876f806f6c936a2c6118d4
JhyunJung <jhyun.rigel@gmail.com>
2023-08-15 21:12:27 +0900

    fix: UserStorages 위치

--- src/main/java/com/side/cooktime/domain/userstorage/model/UserStorages.java
+++ src/main/java/com/side/cooktime/domain/userstorage/model/UserStorages.java
@@ -1,6 +1,9 @@
 package com.side.cooktime.domain.userstorage.model;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,11 +14,13 @@ import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @Getter
+@Embeddable
 public class UserStorages {
+    @OneToMany(mappedBy = "member", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<UserStorage> userStorages = new ArrayList<>();
 
-    public UserStorages(List<UserStorage> list){
-        userStorages = list;
+    public UserStorages(List<UserStorage> userStorages){
+        this.userStorages = userStorages;
     }
 
     public int getSize(){
