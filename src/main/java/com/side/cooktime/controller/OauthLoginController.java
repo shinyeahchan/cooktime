255b5bb25ad1725652b76826e24a4fb7e2620211
ShinYeahChan <syeahc@gmail.com>
2023-08-29 20:36:51 +0900

    feat : 모바일(클라이언트)에서 사용한 Redirect Uri 받아와서 구글 정보 요청

--- src/main/java/com/side/cooktime/controller/OauthLoginController.java
+++ src/main/java/com/side/cooktime/controller/OauthLoginController.java
@@ -8,6 +8,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,14 +25,19 @@ import java.util.Map;
 public class OauthLoginController {
     private final GoogleService googleService;
 
-    //임시
+    @Value("${google.redirectUri}")
+    private String REDIRECT_URI;
+
+    //서버 확인용
     //https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=595159133596-ibjnkksdef8bumsndm9vjn0cd4790jtr.apps.googleusercontent.com&scope=email%20profile&redirect_uri=http://localhost/api/v1/callback
+    //https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=595159133596-ibjnkksdef8bumsndm9vjn0cd4790jtr.apps.googleusercontent.com&scope=email%20profile&redirect_uri=http://ec2-43-202-30-201.ap-northeast-2.compute.amazonaws.com/api/v1/callback
     @GetMapping(value = "/callback")
     public ResponseEntity<LoginResponse> GoogleSignCallback(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String code = request.getParameter("code");
         log.info("## [REQUEST] code : {}", request.getParameter("code"));
         Map<String, String> requestMap = new HashMap<>();
         requestMap.put("authorizationCode", request.getParameter("code"));
+        requestMap.put("redirectUri", REDIRECT_URI);
         return googleLogin(requestMap);
     }
 
@@ -39,7 +45,7 @@ public class OauthLoginController {
     @ResponseBody
     public ResponseEntity<LoginResponse> googleLogin(@RequestBody Map<String, String> requestMap) throws Exception {
         log.info("googleLogin");
-        Member loginMember  = googleService.login(requestMap.get("authorizationCode"));
+        Member loginMember  = googleService.login(requestMap.get("authorizationCode"), requestMap.get("redirectUri"));
         LoginResponse loginResponse = googleService.getJwtResponse(loginMember);
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + loginResponse.getToken());
