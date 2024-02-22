988c486daf880d8bf19cde252ae6d7490dc84382
JhyunJung <jhyun.rigel@gmail.com>
2024-02-22 20:19:18 +0900

    restore

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,11 +4,6 @@ insert into ingredient (category_id, created_at, updated_at, expiration_period, 
 values (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 30, '등심', 'test', 'AMOUNT', 'COLD'),
        (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 30, '안심', 'test2', 'AMOUNT', 'COLD');
 
-insert into ingredient (category_id, created_at, updated_at, expiration_period, name, url, count_type, storage_type)
-values (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 30, '등심', 'test', 'AMOUNT', 'COLD'),
-       (1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 30, '안심', 'test2', 'AMOUNT', 'COLD');
-
-
 # insert into member (created_at, updated_at
 # , first_name, last_name, email, password, provider, provider_id, role)
 #     values(CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 'JaeHyun', 'Jung', 'jhyun.rigel@gmail.com', 'pass_word', 'google', 100371066518631530073, 'USER');
